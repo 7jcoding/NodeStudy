f2d3f1cf3d816f5ca8ca36c75d31a667d58679be
linlvming <linlvming@linggan.com>
2016-06-30 20:21:44 +0800

    添加wget.js文件

--- wget.js
+++ wget.js
@@ -2,7 +2,7 @@
  * Created by LIN on 2016/6/5.
  */
 var nodegrass = require('nodegrass');
-nodegrass.get("http://sendmsg.fanhuan.com/qxt_sms.ashx?api=send&mn=15959273325&ct=发送测试",function(data,status,headers){
+nodegrass.get("http://www.baidu.com",function(data,status,headers){
     console.log(status);
     console.log(headers);
     //console.log(data);
